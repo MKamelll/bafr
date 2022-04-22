6f1cae022f05ca6760cb619e2152d48f4c24c99e
Mina Kamel <minallkamel@gmail.com>
2022-04-22 07:45:43 +0200

    restart by click

--- network.py
+++ network.py
@@ -1,11 +1,12 @@
 from Bafr import Bafr
 from Grid import Grid
+import os
 
 def network_parser(raw):
     g = Grid(raw)
     for i in g:
         if i[2] == "connected":
-            return f"up:{i[3]}"
+            return i[3]
 
 def stength_parser(raw):
     g = Grid(raw)
@@ -13,9 +14,7 @@ def stength_parser(raw):
         if i[0] == "*":
             return f"{i[7]}%"
 
-def network(params=None):
-    if not params: params = []
-
+def query():
     cmd = "nmcli"
     flags = ["device"]
 
@@ -42,4 +41,47 @@ def network(params=None):
     bafr.echo(end=" ")
     bafr2.echo(end="\n")
 
+def device_name_parser(raw):
+    g = Grid(raw)
 
+    for i in g:
+        if i[2] == "connected":
+            return i[0]
+
+def restart():
+    cmd = "nmcli"
+    flags = ["device"]
+
+    bafr = Bafr("network.restart")
+    bafr.set_cmd(cmd)
+    bafr.set_cmd_flags(flags)
+    bafr.set_parser(device_name_parser)
+
+    if not bafr.run():
+        bafr.eecho(end="\n")
+        bafr.exit()
+
+    device_name = bafr.get_parsed_data()
+
+    disconnect_flags = flags + ["disconnect", device_name]
+    connect_flags = flags + ["connect", device_name]
+
+    bafr.set_cmd_flags(disconnect_flags)
+
+    if not bafr.run():
+        bafr.eecho(end="\n")
+        bafr.exit()
+    
+    bafr.set_cmd_flags(connect_flags)
+
+    if not bafr.run():
+        bafr.eecho(end="\n")
+        bafr.exit()
+
+def network(params=None):
+    button = os.getenv("button")
+    if button == "1":
+        restart()
+
+    query()
+   
